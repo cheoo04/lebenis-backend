8867e3f7071c41d65d4381741961d7b431613564
Cheoo04 <yahmardocheek@gmail.com>
2025-11-04 23:39:40 +0000

    feat: Auto-create Driver profile on user registration
    - Add Django signal to automatically create Driver profile when user_type='driver'
    - Ensures all driver registrations have a corresponding Driver model
    - Sets default values: vehicle_type='moto', verification_status='pending'
    - Fixes issue where users could register but have no driver profile

--- backend/apps/drivers/apps.py
+++ backend/apps/drivers/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class DriversConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.drivers'
+    
+    def ready(self):
+        """Import signals when Django starts"""
+        import apps.drivers.signals  # noqa
